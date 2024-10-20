86da57692f16ebd1b97dd0c53b77b6f878fdd594
WH-0501 <wbang8933@163.com>
2024-10-20 20:53:18 +0800

    Site updated: 2024-10-20 20:53:16

--- 2024/09/21/hello-world/zmq-examples/src/hello.c
+++
@@ -1,9 +0,0 @@
-#include <czmq.h>
-
-int main (void) {
-    zsock_t *publisher = zsock_new (ZMQ_PUB);
-    zsock_set_curve_server (publisher, true);
-    puts ("Hello, Curve!");
-    zsock_destroy (&publisher);
-    return 0;
-}
